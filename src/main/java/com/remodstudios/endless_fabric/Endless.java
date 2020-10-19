5eda7dd85f0bb5619f1de5e611b1b46d31403f7f
YTG1234 <54103478+YTG1234@users.noreply.github.com>
2020-10-19 19:22:15 +0300

    It's Javadoc time!

--- src/main/java/com/remodstudios/endless_fabric/Endless.java
+++ src/main/java/com/remodstudios/endless_fabric/Endless.java
@@ -11,12 +11,21 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * The common initializer of Endless.
+ *
+ * @author LePichu
+ * @author YTG1234
+ */
 public class Endless implements ModInitializer {
     public static final String MOD_ID = "endless_fabric";
     public static final String MOD_NAME = "Endless";
 
     public static Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
+    /**
+     * Initializes mod and registers items.
+     */
     @Override
     public void onInitialize() {
         log(Level.INFO, "Initializing");
@@ -25,6 +34,11 @@ public class Endless implements ModInitializer {
         EndlessModItems.registerItems();
     }
 
+    /**
+     * Logs a message to the console with this mod's logger.
+     * @param level The {@linkplain Level log level} to log the message in.
+     * @param message The message to log.
+     */
     public static void log(Level level, String message){
         LOGGER.log(level, message);
     }
--- src/main/java/com/remodstudios/endless_fabric/common/register/EndlessModItems.java
+++ src/main/java/com/remodstudios/endless_fabric/common/register/EndlessModItems.java
@@ -7,6 +7,11 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+/**
+ * A class containing all of the mod's items.
+ *
+ * @author YTG1234
+ */
 public final class EndlessModItems {
     public static final Item TOPAZ_GEM;
 
@@ -14,9 +19,13 @@ public final class EndlessModItems {
         TOPAZ_GEM = new Item(new FabricItemSettings().group(ItemGroup.MISC));
     }
 
+    private EndlessModItems() {
+    }
+
+    /**
+     * Registers all of the mod's items.
+     */
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(Endless.MOD_ID, "topaz_gem"), TOPAZ_GEM);
     }
-
-    private EndlessModItems() {}
 }
