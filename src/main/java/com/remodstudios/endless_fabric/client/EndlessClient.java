0fa825398b44fa7913f3a74e0a127d8469fbc103
YTG1234 <54103478+YTG1234@users.noreply.github.com>
2020-10-20 08:10:46 +0300

    Added a client entrypoint.
    I also added a bunch of folders on my local repo but Git doesn't like to have empty folders.

---
+++ src/main/java/com/remodstudios/endless_fabric/client/EndlessClient.java
@@ -0,0 +1,16 @@
+package com.remodstudios.endless_fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+// Currently this doesn't do anything but it will be useful in the future
+/**
+ * Initializes the client-side stuff.
+ *
+ * @author YTG1234
+ */
+public class EndlessClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
