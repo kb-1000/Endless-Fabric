24955959e231e508e7f74dd57595f8f0105944bb
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-22 11:29:16 +0530

    Topaz block
     Changes to be committed:
    	modified:   src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
    	modified:   src/main/java/com/remodstudios/endless_fabric/item/EndlessModItems.java
    	modified:   src/main/resources/assets/endless_fabric/lang/en_us.json

--- src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
+++ src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
@@ -1,22 +1,23 @@
 package com.remodstudios.endless_fabric.block;
 
 import com.remodstudios.endless_fabric.Endless;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.MaterialColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import static net.minecraft.block.Blocks.END_STONE;
-import static net.minecraft.block.Blocks.IRON_BLOCK;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 
 public class EndlessModBlocks {
-    public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "topaz_ore");
-    public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "finallium_ore");
-    public static final Block RUSTED_IRON_BLOCK = register(new Block(FabricBlockSettings.copyOf(IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3)),"rusted_iron_block");
+    public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(Blocks.END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "topaz_ore");
+    public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(Blocks.END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "finallium_ore");
+    public static final Block RUSTED_IRON_BLOCK = register(new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3)), "rusted_iron_block");
     public static final Block PHAZED_VOID = register(new Block(FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.BLUE_TERRACOTTA).notSolid().allowsMovement().build()).breakByTool(FabricToolTags.SHEARS)), "phazed_void");
+    public static final Block TOPAZ_BLOCK = register(new Block(FabricBlockSettings.copy(Blocks.DIAMOND_BLOCK)), "topaz_block");
 
     private static Block register(Block block, String name) {
         return Registry.register(Registry.BLOCK, new Identifier(Endless.MOD_ID, name), block);
--- src/main/java/com/remodstudios/endless_fabric/item/EndlessModItems.java
+++ src/main/java/com/remodstudios/endless_fabric/item/EndlessModItems.java
@@ -17,6 +17,7 @@ public class EndlessModItems {
     public static final Item FINALLIUM_ORE = register(EndlessModBlocks.FINALLIUM_ORE, "finallium_ore", new Item.Settings().group(ItemGroup.MISC));
     public static final Item PHAZED_VOID = register(EndlessModBlocks.PHAZED_VOID, "phazed_void", new Item.Settings().group(ItemGroup.MISC));
     public static final Item RUSTED_IRON_BLOCK = register(EndlessModBlocks.RUSTED_IRON_BLOCK, "phazed_void", new Item.Settings().group(ItemGroup.MISC));
+    public static final Item TOPAZ_BLOCK = register(EndlessModBlocks.TOPAZ_BLOCK, "topaz_block", new Item.Settings().group(ItemGroup.MISC));
 
     private static Item register(Item item, String name) {
         return Registry.register(Registry.ITEM, new Identifier(Endless.MOD_ID, name), item);
