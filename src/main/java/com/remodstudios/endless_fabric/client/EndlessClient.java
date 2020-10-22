54dc68b7425a6360275c611436fb2eee03238f16
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-22 15:35:10 +0530

    FLuid rendering
     Changes to be committed:
    	modified:   src/main/java/com/remodstudios/endless_fabric/client/EndlessClient.java
    	new file:   src/main/java/com/remodstudios/endless_fabric/client/FluidRenderHelper.java
    	new file:   src/main/resources/assets/endless_fabric/blockstates/molten_topaz.json
    	new file:   src/main/resources/assets/endless_fabric/models/block/molten_topaz.json
    	new file:   src/main/resources/assets/endless_fabric/textures/block/molten_topaz_flow.png
    	new file:   src/main/resources/assets/endless_fabric/textures/block/molten_topaz_flow.png.mcmeta
    	new file:   src/main/resources/assets/endless_fabric/textures/block/molten_topaz_still.png
    	new file:   src/main/resources/assets/endless_fabric/textures/block/molten_topaz_still.png.mcmeta

--- src/main/java/com/remodstudios/endless_fabric/client/EndlessClient.java
+++ src/main/java/com/remodstudios/endless_fabric/client/EndlessClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class EndlessClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+		FluidRenderHelper.MOLTEN_TOPAZ.init();
     }
 }
