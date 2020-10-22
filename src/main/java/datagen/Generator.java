e743a62c0878e0945151f9b658fd70aabf644444
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-22 10:28:53 +0530

    foo

--- src/main/java/datagen/Generator.java
+++ src/main/java/datagen/Generator.java
@@ -47,28 +47,37 @@ public class Generator {
                                                            "  ]\n" +
                                                            "}\n";
 
+    public static void main(String[] args) throws IOException {
+        try (Scanner s = new Scanner(System.in)) {
+            System.out.println("Enter a list of block names separated by a space");
+            String[] blocks = s.nextLine().trim().split(" ");
+            for (String block : blocks) {
+                generate(block);
+            }
+        }
+    }
+
     // IMPORTANT
     // Make sure the working directory is the project root folder
     // Ensure that it is not set the 'run' directory
-    public static void main(String[] args) throws IOException {
-        try (Scanner s = new Scanner(System.in)) {
-            System.out.println("Enter the name of the block");
-            String name = s.next();
-            String blockstate = String.format(DEFAULT_BLOCKSTATE, name);
-            String blockModel = String.format(DEFAULT_BLOCK_MODEL, name);
-            String blockitemModel = String.format(DEFAULT_BLOCKITEM_MODEL, name);
-            String lootTable = String.format(DEFAULT_BLOCK_LOOT_TABLE, name);
-            Path root = Paths.get(System.getProperty("user.dir"));
-            Path assetsDir = root.resolve("src").resolve("main").resolve("resources").resolve("assets").resolve("endless_fabric");
-            Path blockstatePath = assetsDir.resolve("blockstates").resolve(name + ".json");
-            Path blockModelPath = assetsDir.resolve("models").resolve("block").resolve(name + ".json");
-            Path blockitemModelPath = assetsDir.resolve("models").resolve("item").resolve(name + ".json");
-            Path lootTablePath = root.resolve("src").resolve("main").resolve("resources").resolve("data").resolve("endless_fabric").resolve("loot_tables").resolve("blocks").resolve(name + ".json");
-            write(blockstatePath, blockstate);
-            write(blockModelPath, blockModel);
-            write(blockitemModelPath, blockitemModel);
-            write(lootTablePath, lootTable);
-        }
+    public static void generate(String name) throws IOException {
+        long start = System.currentTimeMillis();
+        System.out.println("Generating resources for block: " + name);
+        String blockstate = String.format(DEFAULT_BLOCKSTATE, name);
+        String blockModel = String.format(DEFAULT_BLOCK_MODEL, name);
+        String blockitemModel = String.format(DEFAULT_BLOCKITEM_MODEL, name);
+        String lootTable = String.format(DEFAULT_BLOCK_LOOT_TABLE, name);
+        Path root = Paths.get(System.getProperty("user.dir"));
+        Path assetsDir = root.resolve("src").resolve("main").resolve("resources").resolve("assets").resolve("endless_fabric");
+        Path blockstatePath = assetsDir.resolve("blockstates").resolve(name + ".json");
+        Path blockModelPath = assetsDir.resolve("models").resolve("block").resolve(name + ".json");
+        Path blockitemModelPath = assetsDir.resolve("models").resolve("item").resolve(name + ".json");
+        Path lootTablePath = root.resolve("src").resolve("main").resolve("resources").resolve("data").resolve("endless_fabric").resolve("loot_tables").resolve("blocks").resolve(name + ".json");
+        write(blockstatePath, blockstate);
+        write(blockModelPath, blockModel);
+        write(blockitemModelPath, blockitemModel);
+        write(lootTablePath, lootTable);
+        System.out.println("Finished generating data for block: " + name + " in " + (System.currentTimeMillis() - start) + " ms");
     }
 
     private static void write(Path path, String data) throws IOException {
