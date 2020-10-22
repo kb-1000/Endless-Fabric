af4b5a0d2332fa107c9e94d770dafe5d75d389e2
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-22 14:32:24 +0530

    Move generator
     Changes to be committed:
    	modified:   .editorconfig
    	modified:   build.gradle
    	renamed:    src/main/java/datagen/Generator.java -> datagen/src/main/java/datagen/Generator.java
    	modified:   settings.gradle

--- datagen/src/main/java/datagen/Generator.java
+++ datagen/src/main/java/datagen/Generator.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+@SuppressWarnings("TextBlockMigration")
 public class Generator {
     private static final String DEFAULT_BLOCKSTATE = "{\n" +
                                                      "  \"variants\": {\n" +
