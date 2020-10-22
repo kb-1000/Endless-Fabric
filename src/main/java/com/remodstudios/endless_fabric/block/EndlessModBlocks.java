a0e7d539910481ba1da2938489e6b1a1acbdeeab
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2020-10-22 09:58:28 +0530

    haha yes rusted iron go brrrrrrr

--- src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
+++ src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
@@ -10,10 +10,12 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import static net.minecraft.block.Blocks.END_STONE;
+import static net.minecraft.block.Blocks.IRON_BLOCK;
 
 public class EndlessModBlocks {
     public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "topaz_ore");
     public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "finallium_ore");
+    public static final Block RUSTED_IRON_BLOCK = register(new Block(FabricBlockSettings.copyOf(IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3)),"rusted_iron_block");
     public static final Block PHAZED_VOID = register(new Block(FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.BLUE_TERRACOTTA).notSolid().allowsMovement().build()).breakByTool(FabricToolTags.SHEARS)), "phazed_void");
 
     private static Block register(Block block, String name) {
