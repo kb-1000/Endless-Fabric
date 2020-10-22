cba2dd1317ff131a5b0469edd66401c8dbbb6a11
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2020-10-22 14:22:33 +0530

    topaz block moment

--- src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
+++ src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
@@ -5,19 +5,24 @@ import com.remodstudios.endless_fabric.Endless;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.MaterialColor;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import static net.minecraft.block.Blocks.DIAMOND_BLOCK;
+import static net.minecraft.block.Blocks.END_STONE;
+import static net.minecraft.block.Blocks.IRON_BLOCK;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 
 public class EndlessModBlocks {
-    public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(Blocks.END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "topaz_ore");
-    public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(Blocks.END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "finallium_ore");
-    public static final Block RUSTED_IRON_BLOCK = register(new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3)), "rusted_iron_block");
-    public static final Block PHAZED_VOID = register(new Block(FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.BLUE_TERRACOTTA).notSolid().allowsMovement().build()).breakByTool(FabricToolTags.SHEARS)), "phazed_void");
-    public static final Block TOPAZ_BLOCK = register(new Block(FabricBlockSettings.copy(Blocks.DIAMOND_BLOCK)), "topaz_block");
+    public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2).sounds(BlockSoundGroup.STONE)), "topaz_ore");
+    public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2).sounds(BlockSoundGroup.STONE)), "finallium_ore");
+    public static final Block RUSTED_IRON_BLOCK = register(new Block(FabricBlockSettings.copyOf(IRON_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 3).sounds(BlockSoundGroup.METAL)), "rusted_iron_block");
+    public static final Block PHAZED_VOID = register(new Block(FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.BLUE_TERRACOTTA).notSolid().allowsMovement().build()).breakByTool(FabricToolTags.SHEARS).sounds(BlockSoundGroup.GLASS)), "phazed_void");
+    public static final Block TOPAZ_BLOCK = register(new Block(FabricBlockSettings.copyOf(DIAMOND_BLOCK).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2).sounds(BlockSoundGroup.STONE)),"topaz_block");
 
     private static Block register(Block block, String name) {
         return Registry.register(Registry.BLOCK, new Identifier(Endless.MOD_ID, name), block);
