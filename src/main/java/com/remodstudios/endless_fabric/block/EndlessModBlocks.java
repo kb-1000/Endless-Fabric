2731350d150fb6e2bb57000339305f1309feb9fc
SD <harrypotter.starwars.shrish@gmail.com>
2020-10-22 09:42:56 +0530

    Add phazed void
     Changes to be committed:
    	modified:   src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
    	new file:   src/main/resources/assets/endless_fabric/blockstates/phazed_void.json
    	new file:   src/main/resources/assets/endless_fabric/models/block/phazed_void.json
    	new file:   src/main/resources/assets/endless_fabric/textures/block/phased_void.png

--- src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
+++ src/main/java/com/remodstudios/endless_fabric/block/EndlessModBlocks.java
@@ -2,8 +2,10 @@ package com.remodstudios.endless_fabric.block;
 
 import com.remodstudios.endless_fabric.Endless;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricMaterialBuilder;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
+import net.minecraft.block.MaterialColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,6 +14,7 @@ import static net.minecraft.block.Blocks.END_STONE;
 public class EndlessModBlocks {
     public static final Block TOPAZ_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "topaz_ore");
     public static final Block FINALLIUM_ORE = register(new Block(FabricBlockSettings.copyOf(END_STONE).requiresTool().breakByTool(FabricToolTags.PICKAXES, 2)), "finallium_ore");
+    public static final Block PHAZED_VOID = register(new Block(FabricBlockSettings.of(new FabricMaterialBuilder(MaterialColor.BLUE_TERRACOTTA).notSolid().allowsMovement().build()).breakByTool(FabricToolTags.SHEARS)), "phazed_void");
 
     private static Block register(Block block, String name) {
         return Registry.register(Registry.BLOCK, new Identifier(Endless.MOD_ID, name), block);
